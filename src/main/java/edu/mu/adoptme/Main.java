ac43c47dfc341911bcdf5c3d6f0db9e183bd5b54
Jacob Stirnemann <jacobstirnemann@Jacobs-MacBook-Pro.local>
2025-05-04 15:54:20 -0500

    Created Main.java

---
+++ src/main/java/edu/mu/adoptme/Main.java
@@ -0,0 +1,12 @@
+package edu.mu.adoptme;
+
+import edu.mu.adoptme.controller.AdoptionController;
+
+/**
+ * Main entry point of the application.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new AdoptionController();
+    }
+}
